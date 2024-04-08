afb1992a1d64c605dbeaaf305bf04a7518fc16ea
Mudassir Ali <38592433+MudassirAli94@users.noreply.github.com>
2024-04-08 17:26:40 -0400

    Update homework_extract_script.py

--- homework_extract_script.py
+++ homework_extract_script.py
@@ -1,27 +1,47 @@
 import pandas as pd
 import requests
 from gcp_functions import upload_dataframe_to_gcs
+from tqdm import tqdm
 
-YOUR_BUCKET_NAME =  "mudassir-cis9440-hw"
+YOUR_BUCKET_NAME = "mudassir-cis9440-hw"
 PROJECT_ID = 'dw-group-project'
 
 url = "https://data.lacity.org/resource/2nrs-mtv8.csv"
+api_limit = 1000
+params = {"$limit": api_limit}
 
-response = requests.get(url)
+dfs = []
+offset = 0
 
-if response.status_code == 200:
-    df = pd.read_csv(url)
-    print(df.head())
-else:
-    print("Failed to fetch data. Status code:", response.status_code)
+while True:
+    params["$offset"] = offset
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        df = pd.read_csv(response.url)
+        dfs.append(df)
+        assert len(dfs)
+
+        if len(df) < api_limit:
+            break
+        else:
+            offset += api_limit
+    else:
+        print("Failed to fetch data. Status code:", response.status_code)
+        break
 
 
-for n in df.columns:
-    print(df[n].head())
-    print()
+    tqdm.write(f"Downloaded {offset} rows")
+
+
+final_df = pd.concat(dfs, ignore_index=True)
+
+assert final_df is not None, "Dataframe is empty. Check the data source."
+assert len(final_df) == 925720, "Dataframe is too small. Check the data source."
 
 upload_dataframe_to_gcs(bucket_name=YOUR_BUCKET_NAME,
-                        df=df, file_name='crime_data_raw',
+                        df=final_df,
+                        file_name='crime_data_raw',
                         project_id=PROJECT_ID)
 
 
